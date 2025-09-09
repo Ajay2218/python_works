7095593ded0487357beeb764a0354653c5d32a93
Ajay <ajayramesh788@gmail.com>
2025-09-09 12:29:02 +0530

    patterns

---
+++ nested_loop/pattern.py
@@ -0,0 +1,12 @@
+
+for i in range(1,4):
+
+    for j in range(1,5):
+
+        if j % 2 == 0:
+
+            print(">",end=" ")
+        else:
+            print("<",end=" ")
+    
+    print()
---
+++ nested_loop/triangle_pattern2.py
@@ -0,0 +1,14 @@
+
+
+for i in range(6,0,-1):
+
+    print(i*" *")
+
+
+for row in range(1,7):
+
+    for col in range(row,7):
+
+        print("*",end=" ")
+    
+    print()
